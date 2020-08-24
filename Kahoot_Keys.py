8b9265843e1be1d044d13590ea99a2ab7dc0ec08
Differentunic <66045103+Differentunic@users.noreply.github.com>
2020-08-24 12:41:18 +1000

    Update Kahoot_Keys.py

--- Kahoot_Keys.py
+++ Kahoot_Keys.py
@@ -2,6 +2,28 @@ from os import path
 from pyautogui import leftClick
 from keyboard import is_pressed
 from plyer import notification
+from requests import get
+
+url = ("https://raw.githubusercontent.com/Differentunic/Kahoot_Keys/master/Kahoot_Keys.ico")
+icon_data = get(url, verify=False)
+open('Kahoot_Keys.ico', 'wb').write(icon_data.content)
+
+def start():
+    notification.notify(
+        title="Kahoot Keys",
+        message="Kahoot Keys is running. \n Press (Esc) to quit",
+        app_name="Kahoot Keys",
+        app_icon="Kahoot_Keys.ico"
+    )
+
+
+def quit():
+    notification.notify(
+        title="Kahoot Keys",
+        message="Kahoot Keys has quit.",
+        app_name="Kahoot Keys",
+        app_icon="Kahoot_Keys.ico"
+    )
 
 
 def notification_on_start():
@@ -46,4 +68,7 @@ def key_check():
                 exit()
 
 
-notification_on_start()
+# notification_on_start()
+
+if __name__ == '__main__':
+    start()
